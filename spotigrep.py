29899144452d96ea0a433fd3103d18343506695a
jakob lambert-hartmann <jakob.lamberthartmann.canada@gmail.com>
2020-06-23 19:54:13 +0000

    now saving lyrics files because genius takes very long to download all lyrics

--- spotigrep.py
+++ spotigrep.py
@@ -1,36 +1,93 @@
 import sys
+import os
 import re
 
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
-spotify_api_dir = "./api/spotify/"
+import lyricsgenius
 
-def get_credentials(dir_path):
-    with open(dir_path + "client-id", "r") as id_file, \
-         open(dir_path + "client-secret", "r") as secret_file:
-             return id_file.readline(),secret_file.readline()
+api_dir = "./api/"
+lyrics_dir = "./lyrics/"
+
+def get_credentials():
+    with open(api_dir + "spotify/client-id", "r") as id_file, \
+         open(api_dir + "spotify/client-secret", "r") as secret_file, \
+         open(api_dir + "genius/client-access-token", "r") as token_file:
+             return id_file.readline(),secret_file.readline(),token_file.readline()
 
 
 # initializing api
-sci, scs = get_credentials(spotify_api_dir)
+sci, scs, gct= get_credentials()
+
+#spotify api
 credentials = SpotifyClientCredentials(client_id=sci, client_secret=scs)
 sp = spotipy.Spotify(client_credentials_manager=credentials)
 
+#genius api
+gn = lyricsgenius.Genius(gct)
+gn.verbose=False
 
-def print_tracks(tracks):
-    def print_ctracks(ctracks):
+
+def get_tracks(tracks):
+    def get_ctracks(ctracks, res):
         for i, item in enumerate(ctracks['items']):
             track = item['track']
-            print("%s %s" % (track['artists'][0]['name'], track['name']))
+            artist = track['artists'][0]['name']
+            track_name = track['name']
 
-    print_ctracks(tracks)
+            if artist not in res:
+                res[artist] = set()
+            res[artist].add(track_name)
+        return res
+
+    res = dict()
+    res = get_ctracks(tracks, res)
     while tracks['next']:
         tracks = sp.next(tracks)
-        print_ctracks(tracks)
+        res = get_ctracks(tracks, res)
+    return res 
 
 
+def save_lyrics(tracks, playlist_dir):
+    for artist_name, songs in tracks.items():
+        #create artist dir if not existing
+        clean_artist_name = re.sub("[^A-Za-z0-9]+","","%s" % (artist_name))
+        if not os.path.isdir(playlist_dir + clean_artist_name):
+            os.mkdir(playlist_dir + clean_artist_name)
 
-tracks = sp.playlist_tracks(sys.argv[-1])
-print_tracks(tracks)
+        for song_name in songs:
+            #create song file if it does not exist
+            clean_song_name = re.sub("[^A-Za-z0-9_]+","",song_name)
+            song_path = playlist_dir + clean_artist_name + "/" + clean_song_name
+            if not os.path.exists(song_path):
+                with open(song_path, "w+") as out_file:
+                    song = gn.search_song(song_name, artist_name)
+                    if song is not None :
+                        out_file.write("%s, %s:i\n" % (artist_name, song_name))
+                        out_file.write(song.lyrics)
+                    
 
+
+#create directories if they dont exist
+if not os.path.isdir(lyrics_dir):
+    os.mkdir(lyrics_dir)
+
+playlist_id = sys.argv[-1] 
+
+
+playlist = sp.playlist(playlist_id)
+
+playlist_dir = lyrics_dir + re.sub("[^A-Za-z0-9_]+","",playlist['name']) + "/"
+print(playlist_dir)
+if not os.path.isdir(playlist_dir):
+    os.mkdir(playlist_dir)
+
+
+tracks = get_tracks(playlist['tracks'])
+save_lyrics(tracks, playlist_dir)
+
+
+# for artist_name, track_list in tracks.items():
+#    for track in track_list:
+#       print("%s , %s" % (artist_name,track))
